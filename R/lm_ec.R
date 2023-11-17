5effec298c59ed34e7a47f4f424bc553a158f65c
Elena Chun <elenach@umich.edu>
2023-11-17 17:20:34 -0500

    Help pages and documentation

--- R/lm_ec.R
+++ R/lm_ec.R
@@ -1,17 +1,28 @@
 #Function to perform linear regression given formula and data
+#' Create linear regression model
+#'
+#' @param formula The formula of the linear regression model
+#' @param data The data the model will be fit to
+#'
+#' @return The coefficients of the model
+#' @export
+#'
+#' @examples
+#' data(mtcars)
+#' lm_ec(mpg ~ hp + wt, data = mtcars)
 lm_ec <- function(formula, data) {
   # Get response and predictor variables from the formula
   response <- as.character(formula[[2]])
   predictors <- as.character(formula[[3]])
-  
+
   # Create a design matrix from the given formula and data
   design_matrix <- model.matrix(formula, data)
-  
+
   # Linear regression
   response_vec <- data[[response]]
   coefficients <- solve(t(design_matrix) %*% design_matrix) %*% t(design_matrix) %*% response_vec
   colnames(coefficients) <- "Estimate"
-  
+
   # Build linear regression model output
   model <- list(
     call = match.call(),
@@ -24,7 +35,8 @@ lm_ec <- function(formula, data) {
     df.residual = length(response_vec) - qr(design_matrix)$rank,
     df = nrow(data) - (ncol(design_matrix)-1) - 1
   )
-  
+
   class(model) <- "lm"
   return(model)
+
 }
--- R/lm_summ_ec.R
+++ R/lm_summ_ec.R
@@ -1,9 +1,20 @@
 # Function to summarize the residuals, coefficients, model fit from the linear regression output
+#' Create model summary
+#'
+#' @param model The model output from lm_ec function
+#'
+#' @return A sumary of the residuals, coefficients and model fit
+#' @export
+#'
+#' @examples
+#' data(mtcars)
+#' m1 <- lm_ec(mpg ~ hp + wt, data = mtcars)
+#' lm_summ_ec(m1)
 lm_summ_ec <- function(model) {
   # Get residuals and design matrix from the model
   residuals <- model$residuals
   design_matrix <- model$design_matrix
-  
+
   # Calculate the minimum, 1st quartile, median, 3rd quartile, maximum of the distribution of residuals
   residuals_summ <- c(
     "Min" = min(residuals),
@@ -12,57 +23,57 @@ lm_summ_ec <- function(model) {
     "0" = quantile(residuals, 0.75),
     "Max" = max(residuals)
   )
-  
-  
+
+
   # Calculate the standard error of the coefficients
   # residual sum of squares
   rss <- sum(residuals^2)
-  
+
   # variance-covariance matrix
   var_cov <- (rss / model$df.residual) * solve(t(design_matrix) %*% design_matrix)
-  
+
   # standard errors of coefficients
   std_error <- sqrt(diag(var_cov))
-  
+
   # Calculate t statistic of the predictors
   t_value <- model$coefficients / std_error
-  
+
   # Calculate p value of the t statistic of the predictors
   p_value <- pt(q = abs(t_value), df = model$df, lower.tail = FALSE)
-  
+
   # Summarize the coefficients
   coef_summ <- cbind(model$coefficients, std_error, t_value, p_value)
   colnames(coef_summ) <- c("Estimate", "Std. Error", "t value", "p value")
-  
-  
+
+
   # Assess model fit
   # Calculate Residual Standard Error (RSE)
   rse <- round(sqrt(rss / model$df.residual), digits=3)
   rse_str <- paste0("Residual Standard Error: ", rse, " on ", model$df, " degrees of freedom")
-  
+
   # Calculate multiple r-squared
   # total sum of squares
   tss <- sum((model$fitted.values - mean(model$fitted.values))^2)
   # Multiple R-squared
   mult_r_sq <- 1 - rss / tss
   mult_r_sq_str <- paste0("Multiple R-squared: ", mult_r_sq)
-  
+
   # Calculate adjusted r-squared
   n <- nrow(design_matrix)
   k <- ncol(design_matrix) - 1
   r_sq <- 1 - (rss / sum((model$coefficients - mean(model$coefficients))^2))
   adj_r_sq <- 1 - ((1 - r_sq) * (n - 1) / (n - k - 1))
   adj_r_sq_str <- paste0("Adjusted R-squared: ", adj_r_sq)
-  
+
   # Calculate F-statistic of the model
   df_regr <- model$rank - 1 #regression df
   df_res <- model$df.residual #residual df
   Fstat <- ((sum(model$coefficients^2) / df_regr) / (rss / df_res))
-  
+
   # Calculate p-value for the F-statistic
   p_value_F <- pf(Fstat, df_regr, df_res, lower.tail = FALSE)
   Fstat_str <- paste0("F-statistic: ", Fstat, " on ", df_regr, " and ", df_res, " DF, p-value: ", p_value_F)
-  
+
   # Build summary output
   summ <- list(
     Call = model$call,
@@ -70,6 +81,6 @@ lm_summ_ec <- function(model) {
     Coefficients = coef_summ,
     Model.Fit = paste(c(rse_str, mult_r_sq_str, adj_r_sq_str,Fstat_str), sep = "/n")
   )
-  
+
   return(summ)
 }
